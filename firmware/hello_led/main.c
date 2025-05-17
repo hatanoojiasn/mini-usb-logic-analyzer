63f76b6406a09610e44e2842dc1872c6db057e1c
hatanoojiasn <hatanoarata@gmail.com>
2025-05-17 14:40:07 +0900

    Lチカ確認

---
+++ firmware/hello_led/main.c
@@ -0,0 +1,11 @@
+#include <Arudino.h>
+#define LED_BUILTIN 15
+int main(void){
+    pinMode(LED_BUILTIN, OUTPUT);
+    while(1){
+        digitalWrite(LED_BUILTIN, HIGH);
+        delay(1000);
+        digitalWrite(LED_BUILTIN, LOW);
+        delay(1000);
+    }
+}
